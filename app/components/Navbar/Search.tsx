36808dc09011075ba25481a42f7a29fbb99f18e5
MrTban <esteman214@gmail.com>
2023-05-31 07:57:16 -0500

    barra de busqueda

---
+++ app/components/Navbar/Search.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { BiSearch } from 'react-icons/bi'
+
+const Search = () => {
+	return (
+		<div className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+			<div className='flex flex-row items-center justify-between'>
+				<div className='text-sm font-semibold px-6'>COOL</div>
+				<div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
+					Travels
+				</div>
+				<div className='text-sm pl-6 pr-2 text-gray-500 flex flex-row items-center gap-3'>
+					<div className='hidden sm:block'>Search</div>
+					<div className='p-2 rounded-full text-white bg-cyan-400'>
+						<BiSearch size={14} color='black' />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Search
