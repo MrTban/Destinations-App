db8fda4a3455a1529f9d425c35a8d5b685c7fc63
MrTban <esteman214@gmail.com>
2023-05-31 16:51:33 -0500

    render login modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar/Narbar'
 import ClientOnly from './components/ClientOnly'
+import LoginModal from './components/Modals/LoginModal'
 import RegisterModal from './components/Modals/RegisterModal'
 import ToasterProvider from './providers/ToasterProvider'
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={font.className}>
 				<ClientOnly>
 					<ToasterProvider />
+					<LoginModal />
 					<RegisterModal />
 					<Navbar />
 				</ClientOnly>
