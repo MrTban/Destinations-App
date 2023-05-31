4a8db6393726bf61b59dde26c6e51191f32df6d8
MrTban <esteman214@gmail.com>
2023-05-31 08:32:38 -0500

    cambio de import en menuItem

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -3,7 +3,7 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import { useCallback, useState } from 'react'
-import MenuItem from '../MenuItem'
+import MenuItem from './MenuItem'
 
 const UserMenu = () => {
 	const [isOpen, setIsOpen] = useState(false)
