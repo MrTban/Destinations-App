1549d73b0b6fdcfe75e183538801b8a959f8ae03
MrTban <esteman214@gmail.com>
2023-06-01 11:18:30 -0500

    crear componente Category

---
+++ app/components/Navbar/Categories.tsx
@@ -0,0 +1,23 @@
+import Container from '../Container'
+import CategoryBox from '../CategoryBox'
+import { categories } from './categorias'
+
+const Categories = () => {
+	return (
+		<Container>
+			<div className='flex flex-row items-center justify-between pt-4 overflow-x-auto'>
+				{categories.map((item) => (
+					<CategoryBox
+						key={item.label}
+						label={item.label}
+						// description={item.description}
+						// selected={category === item.label}
+						icon={item.icon}
+					/>
+				))}
+			</div>
+		</Container>
+	)
+}
+
+export default Categories
