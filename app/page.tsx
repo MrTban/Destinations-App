a3af436db29b7d38506358471739113e3cb8e812
MrTban <esteman214@gmail.com>
2023-06-01 19:36:09 -0500

    currentUser at card

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import getCurrentUser from './actions/getCurrentUser'
 import getListings from './actions/getListings'
+
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState'
@@ -6,6 +8,7 @@ import ListingCard from './components/Listings/ListingCard'
 
 export default async function Home() {
 	const listings = await getListings()
+	const currentUser = await getCurrentUser()
 
 	if (listings.length === 0) {
 		return (
@@ -19,7 +22,9 @@ export default async function Home() {
 			<Container>
 				<div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
 					{listings.map((listing: any) => {
-						return <ListingCard key={listing.id} data={listing} />
+						return (
+							<ListingCard currentUser={currentUser} key={listing.id} data={listing} />
+						)
 					})}
 				</div>
 			</Container>
