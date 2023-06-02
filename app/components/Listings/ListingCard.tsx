30fea761f5e2d46d44d32d2a31ec49e88d51dd9a
MrTban <esteman214@gmail.com>
2023-06-02 08:08:59 -0500

    render destinos

--- app/components/Listings/ListingCard.tsx
+++ app/components/Listings/ListingCard.tsx
@@ -5,6 +5,9 @@ import { Listing } from '@prisma/client'
 
 import { SafeUser } from '@/app/types'
 import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
+import Image from 'next/image'
+import HeartButton from '../HeartButton'
 
 interface ListingCardProps {
 	data: Listing
@@ -20,7 +23,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
 	onAction,
 	disabled,
 	actionLabel,
-	actionId,
+	actionId = '',
 	currentUser,
 }) => {
 	const router = useRouter()
@@ -28,9 +31,37 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
 	const location = getByValue(data.locationValue)
 
+	const handleCancel = useCallback(
+		(e: React.MouseEvent<HTMLButtonElement>) => {
+			e.stopPropagation()
+
+			if (disabled) {
+				return
+			}
+
+			onAction?.(actionId)
+		},
+		[onAction, actionId, disabled]
+	)
+
 	return (
-		<div>
-			<div>Card</div>
+		<div
+			onClick={() => router.push(`/listings/${data.id}`)}
+			className='col-span-1 cursor-pointer group'
+		>
+			<div className='flex flex-col gap-2 w-full'>
+				<div className='aspect-square w-full- relative overflow-hidden rounded-xl'>
+					<Image
+						fill
+						alt='Listing'
+						src={data.imageSrc}
+						className='object-cover h-full w-full group-hover:scale-110 transition'
+					/>
+					<div className='absolute top-3 right-3'>
+						<HeartButton listingId={data.id} currentUser={currentUser} />
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
