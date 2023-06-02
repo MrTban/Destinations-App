549d16b3dc08f7862593396a91ae58f93ba3eda7
MrTban <esteman214@gmail.com>
2023-06-01 19:36:59 -0500

    interface and any functions

--- app/components/Listings/ListingCard.tsx
+++ app/components/Listings/ListingCard.tsx
@@ -1,9 +1,36 @@
 'use client'
 
-const ListingCard = () => {
+import useCountries from '@/app/hooks/useCountries'
+import { Listing } from '@prisma/client'
+
+import { SafeUser } from '@/app/types'
+import { useRouter } from 'next/navigation'
+
+interface ListingCardProps {
+	data: Listing
+	onAction?: (id: string) => void
+	disabled?: boolean
+	actionLabel?: string
+	actionId?: string
+	currentUser?: SafeUser | null
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+	data,
+	onAction,
+	disabled,
+	actionLabel,
+	actionId,
+	currentUser,
+}) => {
+	const router = useRouter()
+	const { getByValue } = useCountries()
+
+	const location = getByValue(data.locationValue)
+
 	return (
 		<div>
-			<div></div>
+			<div>Card</div>
 		</div>
 	)
 }
