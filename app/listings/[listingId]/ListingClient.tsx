d066839542bb8c754a0bbda2f56c509a1d9229ac
MrTban <esteman214@gmail.com>
2023-06-02 10:20:30 -0500

    listing individual info

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -4,8 +4,10 @@ import { SafeListing, SafeUser } from '@/app/types'
 
 import { useMemo } from 'react'
 import { categories } from '@/app/components/Navbar/categorias'
+
 import Container from '@/app/components/Container'
 import ListingHead from '@/app/components/Listings/ListingHead'
+import ListingInfo from '@/app/components/Listings/ListingInfo'
 
 interface ListingClientProps {
 	listing: SafeListing & {
@@ -30,6 +32,14 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing, currentUser }) =
 						id={listing.id}
 						currentUser={currentUser}
 					/>
+					<div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+						<ListingInfo
+							user={listing.user}
+							category={category}
+							description={listing.description}
+							locationValue={listing.locationValue}
+						/>
+					</div>
 				</div>
 			</div>
 		</Container>
