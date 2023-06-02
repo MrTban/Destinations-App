0c5b29abf8316c945971c38ed7aecbaa665f6f64
MrTban <esteman214@gmail.com>
2023-06-01 19:07:47 -0500

    render listings and create listing card

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import getListings from './actions/getListings'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState'
+import ListingCard from './components/Listings/ListingCard'
 
-export default function Home() {
-	const isEmpty = true
+export default async function Home() {
+	const listings = await getListings()
 
-	if (isEmpty) {
+	if (listings.length === 0) {
 		return (
 			<ClientOnly>
-				<EmptyState />
+				<EmptyState showReset />
 			</ClientOnly>
 		)
 	}
@@ -16,7 +18,9 @@ export default function Home() {
 		<ClientOnly>
 			<Container>
 				<div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-					<div>Mis futuros destinos</div>
+					{listings.map((listing: any) => {
+						return <ListingCard key={listing.id} data={listing} />
+					})}
 				</div>
 			</Container>
 		</ClientOnly>
