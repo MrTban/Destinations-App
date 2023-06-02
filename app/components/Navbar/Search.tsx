38228b0662c256eb21b8161f0ade8661984b9c74
MrTban <esteman214@gmail.com>
2023-06-02 16:55:14 -0500

    dynamic label at searbar

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,11 +1,26 @@
 'use client'
 
+import { useMemo } from 'react'
 import { BiWorld } from 'react-icons/bi'
+import { useSearchParams } from 'next/navigation'
 
 import useSearchModal from '@/app/hooks/useSearchModal'
+import useCountries from '@/app/hooks/useCountries'
 
 const Search = () => {
 	const searchModal = useSearchModal()
+	const params = useSearchParams()
+	const { getByValue } = useCountries()
+
+	const locationValue = params?.get('locationValue')
+
+	const locationLabel = useMemo(() => {
+		if (locationValue) {
+			return getByValue(locationValue as string)?.label
+		}
+
+		return 'Ubicación'
+	}, [getByValue, locationValue])
 
 	return (
 		<div
@@ -15,10 +30,10 @@ const Search = () => {
 			<div className='flex flex-row items-center justify-between'>
 				<div className='text-sm font-semibold px-6'>COOL</div>
 				<div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
-					Destinos
+					Destino
 				</div>
 				<div className='text-sm pl-6 pr-2 text-gray-500 flex flex-row items-center gap-3'>
-					<div className='hidden sm:block'>Ubicación</div>
+					<div className='hidden sm:block'>{locationLabel}</div>
 					<div className='p-1 rounded-full text-white bg-cyan-400'>
 						<BiWorld size={18} color='black' />
 					</div>
